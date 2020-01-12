b574888182f969c5bd9be391cfa90fa0348eedfc
Igor T <ityulkanov@gmail.com>
2020-01-12 20:05:22 +0300

    Homework 1. Add HelloOtus Class

---
+++ HW01-maven/src/main/java/HelloOtus.java
@@ -0,0 +1,19 @@
+import com.google.common.base.MoreObjects;
+
+public class HelloOtus {
+    private long id;
+    private String name;
+
+    public HelloOtus(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("name", name)
+                .toString();
+    }
+}
