868c41b849071db00f930bb375061f70bc201ed4
Igor <ityulkanov@gmail.com>
2020-01-21 00:36:25 +0300

    Add finction works

---
+++ hw02-arraylist/src/main/java/com/ityulkanov/generics/Main.java
@@ -0,0 +1,18 @@
+package com.ityulkanov.generics;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        DIYArrayList diyArrayList = new DIYArrayList();
+        for (int i = 0; i < 50; i++) {
+            diyArrayList.add(new Date());
+
+        }
+//        diyArrayList.remove(1);
+        System.out.println(diyArrayList.get(0));
+        System.out.println(diyArrayList.get(1));
+
+        System.out.println("Testing what's up ");
+    }
+}
