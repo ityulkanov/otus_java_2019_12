f92706f3f338fd71b20abb4e555490404172ffbe
Igor Tyulkanov <igor.tyulkanov@smartbics.com>
2020-02-10 15:53:10 +0300

    Added sample code for gc

---
+++ hw03-gc/src/main/java/com/ityulkanov/BenchmarkMBean.java
@@ -0,0 +1,6 @@
+package com.ityulkanov;
+
+public interface BenchmarkMBean {
+    int getSize();
+    void setSize(int size);
+}
---
+++ hw03-gc/src/main/java/com/ityulkanov/GcDemo.java
@@ -0,0 +1,62 @@
+package com.ityulkanov;
+
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.NotificationEmitter;
+import javax.management.NotificationListener;
+import javax.management.ObjectName;
+import javax.management.openmbean.CompositeData;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+public class GcDemo {
+    public static void main(String[] args) throws NotCompliantMBeanException, InstanceAlreadyExistsException, MBeanRegistrationException, MalformedObjectNameException, InterruptedException {
+        System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
+        switchOnMonitoring();
+        long beginTime = System.currentTimeMillis();
+
+        int size = 5 * 1000 * 1000;
+        int loopCounter = 1000;
+
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("com.ityulkanov:type=Benchmark");
+        Benchmark mbean = new Benchmark(loopCounter);
+        mbs.registerMBean(mbean, name);
+        mbean.setSize(size);
+        mbean.run();
+
+        System.out.println("time: " + (System.currentTimeMillis() - beginTime) / 1000);
+
+    }
+
+    private static void switchOnMonitoring() {
+        List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcbean : gcbeans) {
+            System.out.println("GC name: " + gcbean.getName());
+
+            NotificationEmitter emitter = (NotificationEmitter) gcbean;
+            NotificationListener listener = ((notification, handback) -> {
+                if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+                    String gcName = info.getGcName();
+                    String gcAction = info.getGcAction();
+                    String gcCause = info.getGcCause();
+
+                    long startTime = info.getGcInfo().getStartTime();
+                    long duration = info.getGcInfo().getDuration();
+                    System.out.println("Start : " + startTime + " ms, name: " + gcName + " ,action" + gcAction + " ,gcCause " + gcCause + "("
+                            + duration + " ms)");
+
+                }
+            });
+            emitter.addNotificationListener(listener, null, null);
+        }
+    }
+}
+
