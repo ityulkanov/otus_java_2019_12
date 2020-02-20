de92f4feca681bc146277f97d3f77a0c6a518ed0
Igor Tyulkanov <igor.tyulkanov@smartbics.com>
2020-02-20 13:16:43 +0300

    cleanup

--- hw03-gc/src/main/java/com/ityulkanov/Benchmark.java
+++ hw03-gc/src/main/java/com/ityulkanov/Benchmark.java
@@ -16,9 +16,7 @@ public class Benchmark implements BenchmarkMBean {
     void run() throws InterruptedException {
         for (int idx = 0; idx < loopCounter; idx++) {
             int local = size;
-//            List<String> array = new ArrayList<>(local);
             for (int i = 0; i < local-1; i++) {
-//                array.add(i, new String(new char[0]));
                 int[] array = new int[size];
                 size = size + 10000;
             }
@@ -26,9 +24,6 @@ public class Benchmark implements BenchmarkMBean {
             if(loopCounter % 100 == 0) {
                 System.out.println("Available memory (in bytes): " + Runtime.getRuntime().freeMemory());
             }
-//            for (int i = size / 2; i > 0; i--) {
-//                array.remove(i);
-//            }
 
         }
     }
